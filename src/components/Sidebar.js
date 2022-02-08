5080f3381f0feb17709f51aa3936e2d17bd6984e
tonirul <tonirulislam@outlook.com>
2022-02-09 00:58:45 +0530

    started building the sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DashboardToggle from './dashboard/DashboardToggle';
+
+export const Sidebar = () => {
+  return (
+    <div className='h-100 pt-2'>
+
+      <div>
+        <DashboardToggle />
+      </div>
+        bottom
+    </div>
+  );
+};
---
+++ src/components/dashboard/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      Hello!!! this is index of dashboard
+    </div>
+  );
+};
+
+export default Dashboard;
