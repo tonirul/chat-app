76c68bb0b3bbcc76acde86c2007484129396f705
tonirul <tonirulislam@outlook.com>
2022-02-08 12:20:59 +0530

    initialized firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: "AIzaSyAnFPJERQkrHm7PAh4wJW3Dq0oXhzc8q0M",
+  authDomain: "chat-web-app-90c7c.firebaseapp.com",
+  databaseURL: "https://chat-web-app-90c7c-default-rtdb.firebaseio.com",
+  projectId: "chat-web-app-90c7c",
+  storageBucket: "chat-web-app-90c7c.appspot.com",
+  messagingSenderId: "117955760640",
+  appId: "1:117955760640:web:cb8cce2e5a932feb153ee5"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(config);
