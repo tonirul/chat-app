404673de0774e5343e416b21fecea86369c8f0be
tonirul <tonirulislam@outlook.com>
2022-02-10 00:49:16 +0530

    minor bug fix

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import CreateRoomBtnModal from './dashboard/CreateRoomBtnModal';
+import React, { useRef, useState, useEffect } from 'react';
+import { Divider } from 'rsuite';
 import DashboardToggle from './dashboard/DashboardToggle';
+import CreateRoomBtnModal from './dashboard/CreateRoomBtnModal';
+import ChatRoomList from './rooms/ChatRoomList';
 
-export const Sidebar = () => {
+const Sidebar = () => {
+  const topSidebarRef = useRef();
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    if (topSidebarRef.current) {
+      setHeight(topSidebarRef.current.scrollHeight);
+    }
+  }, [topSidebarRef]);
+
   return (
     <div className="h-100 pt-2">
-      <div>
+      <div ref={topSidebarRef}>
         <DashboardToggle />
         <CreateRoomBtnModal />
+        <Divider style={{ margin: 0, padding: '30px 0' }}>
+          Join conversation
+        </Divider>
       </div>
-      bottom
+      <ChatRoomList aboveElHeight={height} />
     </div>
   );
 };
+
+export default Sidebar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Col, Grid, Row } from 'rsuite';
-import { Sidebar } from '../components/Sidebar';
+import Sidebar from '../components/Sidebar';
 
 const Home = () => {
   return (
-    <Grid fluid className='h-100'>
-      <Row>
-        <Col xs={24} md={8}>
-          <Sidebar/>
+    <Grid fluid className="h-100">
+      <Row className="h-100">
+        <Col xs={24} md={8} className="h-100">
+          <Sidebar />
         </Col>
       </Row>
     </Grid>
@@ -15,4 +15,3 @@ const Home = () => {
 };
 
 export default Home;
-
