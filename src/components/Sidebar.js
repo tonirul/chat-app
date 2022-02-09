226e0b943d720212aa3d3a028db2be0c503b615d
tonirul <tonirulislam@outlook.com>
2022-02-09 23:49:56 +0530

    added  button to create new chat room

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import CreateRoomBtnModal from './dashboard/CreateRoomBtnModal';
 import DashboardToggle from './dashboard/DashboardToggle';
 
 export const Sidebar = () => {
   return (
-    <div className='h-100 pt-2'>
-
+    <div className="h-100 pt-2">
       <div>
         <DashboardToggle />
+        <CreateRoomBtnModal />
       </div>
-        bottom
+      bottom
     </div>
   );
 };
