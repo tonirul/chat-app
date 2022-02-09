37a845915c242ce07cd976cdd2a0c22dbc003fcf
tonirul <tonirulislam@outlook.com>
2022-02-09 21:57:01 +0530

    uploaded user avatar

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,15 +1,16 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
+import 'firebase/storage';
 
 const config = {
-  apiKey: "AIzaSyAnFPJERQkrHm7PAh4wJW3Dq0oXhzc8q0M",
-  authDomain: "chat-web-app-90c7c.firebaseapp.com",
-  databaseURL: "https://chat-web-app-90c7c-default-rtdb.firebaseio.com",
-  projectId: "chat-web-app-90c7c",
-  storageBucket: "chat-web-app-90c7c.appspot.com",
-  messagingSenderId: "117955760640",
-  appId: "1:117955760640:web:cb8cce2e5a932feb153ee5"
+  apiKey: 'AIzaSyAnFPJERQkrHm7PAh4wJW3Dq0oXhzc8q0M',
+  authDomain: 'chat-web-app-90c7c.firebaseapp.com',
+  databaseURL: 'https://chat-web-app-90c7c-default-rtdb.firebaseio.com',
+  projectId: 'chat-web-app-90c7c',
+  storageBucket: 'chat-web-app-90c7c.appspot.com',
+  messagingSenderId: '117955760640',
+  appId: '1:117955760640:web:cb8cce2e5a932feb153ee5',
 };
 
 // Initialize Firebase
@@ -17,3 +18,4 @@ const app = firebase.initializeApp(config);
 
 export const auth = app.auth();
 export const database = app.database();
+export const storage = app.storage();
