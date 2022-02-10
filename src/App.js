f2a6b7e51fad76509215757d47ebb6933d499749
tonirul <tonirulislam@outlook.com>
2022-02-10 10:55:43 +0530

    Created layout for homepage and created chat page

--- src/App.js
+++ src/App.js
@@ -5,17 +5,16 @@ import './styles/main.scss';
 import { Switch } from 'react-router';
 import SignIn from './pages/SignIn';
 import PrivateRoute from './components/PrivateRoute';
-import Home from './pages/Home';
+import Home from './pages/Home/index';
 import PublicRoute from './components/PublicRoute';
 import { ProfileProvider } from './context/profile.context';
-
 
 function App() {
   return (
     <ProfileProvider>
       <Switch>
         <PublicRoute path="/signin">
-          <SignIn/>
+          <SignIn />
         </PublicRoute>
         <PrivateRoute path="/">
           <Home />
---
+++ src/pages/Home/Chat.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Chat = () => {
+  return <div>Hello i am the chat.js page</div>;
+};
+
+export default Chat;
