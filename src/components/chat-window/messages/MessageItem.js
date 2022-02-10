d291d0975bb97dc57c36dd42df42128db3075cde
tonirul <tonirulislam@outlook.com>
2022-02-10 16:46:20 +0530

    displayed user database with modal window

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import TimeAgo from 'timeago-react';
 import ProfileAvatar from '../../ProfileAvatar';
+import ProfileInfoBtnModal from './ProfileInfoBtnModal';
 
 const MessageItem = ({ message }) => {
   const { author, createdAt, text } = message;
 
   return (
     <li className="padded mb-1">
-      <div className='="d-flex align-items-center font-bolder mb-1'>
+      <div className="d-flex align-items-center font-bolder mb-1">
         <ProfileAvatar
           src={author.avatar}
           name={author.name}
@@ -15,7 +16,11 @@ const MessageItem = ({ message }) => {
           size="xs"
         />
 
-        <span className="ml-2">{author.name}</span>
+        <ProfileInfoBtnModal
+          profile={author}
+          appearance="link"
+          className="p-0 ml-1 text-black"
+        />
         <TimeAgo
           datetime={createdAt}
           className="font-normal text-black-45 ml-2"
