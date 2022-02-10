640ce70f1055c2709dd5a63272acb6f7e9c61878
tonirul <tonirulislam@outlook.com>
2022-02-10 11:47:22 +0530

    created current room contexts with the usecontext selector

--- src/pages/Home/Chat.js
+++ src/pages/Home/Chat.js
@@ -5,6 +5,7 @@ import ChatTop from '../../components/chat-window/top';
 import ChatBottom from '../../components/chat-window/bottom';
 import Messages from '../../components/chat-window/messages';
 import { useRooms } from '../../context/rooms.context';
+import { CurrentRoomProvider } from '../../context/current-rrom.context';
 
 const Chat = () => {
   const { chatId } = useParams();
@@ -21,8 +22,15 @@ const Chat = () => {
     return <h6 className="text-center mt-page">Chat {chatId} not found</h6>;
   }
 
+  const { name, description } = currentRoom;
+
+  const currentRoomData = {
+    name,
+    description,
+  };
+
   return (
-    <>
+    <CurrentRoomProvider data={currentRoomData}>
       <div className="chat-top">
         <ChatTop />
       </div>
@@ -32,7 +40,7 @@ const Chat = () => {
       <div className="chat-bottom">
         <ChatBottom />
       </div>
-    </>
+    </CurrentRoomProvider>
   );
 };
 
